a4d2a02dd19c016d5cbc193ce831c1484ac87c07
eunhyang231 <72whdmsgid@gmail.com>
2026-07-03 21:28:45 +0900

    Add files via upload

--- shinju-anchor-homepage/js/layout.js
+++ shinju-anchor-homepage/js/layout.js
@@ -1,15 +1,27 @@
 async function loadLayout() {
+  const isProductPage = location.pathname.includes("/products/");
+  const basePath = isProductPage ? "../" : "";
+
   const header = document.getElementById("header");
   const footer = document.getElementById("footer");
 
   if (header) {
-    const res = await fetch("includes/header.html");
-    header.innerHTML = await res.text();
+    const headerRes = await fetch(basePath + "includes/header.html");
+    let headerHtml = await headerRes.text();
+
+    headerHtml = headerHtml.replaceAll('href="', `href="${basePath}`);
+    headerHtml = headerHtml.replaceAll('src="images/', `src="${basePath}images/`);
+
+    header.innerHTML = headerHtml;
   }
 
   if (footer) {
-    const res = await fetch("includes/footer.html");
-    footer.innerHTML = await res.text();
+    const footerRes = await fetch(basePath + "includes/footer.html");
+    let footerHtml = await footerRes.text();
+
+    footerHtml = footerHtml.replaceAll('href="', `href="${basePath}`);
+
+    footer.innerHTML = footerHtml;
   }
 }
 
